49808b2827d4442bdc19f58d3c5e3bdd006372c5
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-06-29 02:56:06 +0000

    Fix linters

--- src/display.js
+++ src/display.js
@@ -2,32 +2,33 @@
 /* eslint-disable no-use-before-define */
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/';
 
-const container = document.querySelector('#main');
-const movieEl = document.createElement('div');
-movieEl.classList.add('movie');
+const container = document.querySelector('#popular-section');
 
 const display = (movies) => {
   movies.forEach((movie) => {
-    const {
-      title, poster_path, vote_average, overview,
-    } = movie;
+    const { title, poster_path, vote_average } = movie;
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
     movieEl.innerHTML = `
         <img src="${IMG_PATH + poster_path}" alt="${title}">
         <div class="movie-info">
-      <h3>${title}</h3>
-      <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+          <h4>${title}</h4>
         </div>
-        <div class="overview">
-      <h3>Overview</h3>
-      ${overview}
-    </div>
-    `;
+  <div class="interact">
+          <button type="button" class="btn btn-info">Comment <i class="bi bi-chat"></i></button>
+          <span class="${getClassByRate(vote_average)} span">
+          <i class="bi bi-heart unlike"></i>
+          <i class="bi bi-heart-fill d-none like"></i>
+          ${vote_average}
+        </span>
+  </div>
+      `;
     container.appendChild(movieEl);
   });
 };
 
 function getClassByRate(vote) {
-  if (vote >= 8) {
+  if (vote >= 7) {
     return 'green';
   } if (vote >= 5) {
     return 'orange';
--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 import './styles/style.scss';
 import logo from './assets/logo.svg';
-import { getMovies, API_URL, SEARCH_API } from './getMovies.js';
+import { getMovies, API_URL } from './getMovies.js';
 
 getMovies(API_URL);
-const form = document.getElementById('form');
-const search = document.getElementById('search');
 const logoIcon = document.getElementById('logo');
 logoIcon.src = logo;
-
-// Add Event Lister to form For Serch inputs
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const searchTerm = search.value;
-
-  if (searchTerm && searchTerm !== '') {
-    getMovies(SEARCH_API + searchTerm);
-
-    search.value = '';
-  } else {
-    window.location.reload();
-  }
-});
