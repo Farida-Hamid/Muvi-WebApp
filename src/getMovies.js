b2e4b8af40f243716a46e9e7782c1537998bac27
Farida-Hamid <faridaaddala@gmail.com>
2022-06-29 13:03:27 +0300

    Fetch movies from API

---
+++ src/getMovies.js
@@ -0,0 +1,11 @@
+import display from './display.js';
+
+export const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=24997fed86518e9d29afc6b7f7510f37&page=1';
+export const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=24997fed86518e9d29afc6b7f7510f37&query="';
+
+// Get Initial Movies
+export async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  display((data.results));
+}
