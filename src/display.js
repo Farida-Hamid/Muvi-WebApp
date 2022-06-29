b44502df8d0b424d757795458f448525f7569cb2
Farida-Hamid <faridaaddala@gmail.com>
2022-06-29 13:04:32 +0300

    Display fetched movies

--- src/display.js
+++ src/display.js
@@ -1,18 +1,39 @@
-const container = document.querySelector('#main');
+/* eslint-disable camelcase */
+/* eslint-disable no-use-before-define */
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/';
 
-export const display = () => {
-  console.log('called');
+const container = document.querySelector('#popular-section');
 
-  container.innerHTML += `    <div class="movie">
-  <img class="cinema" src="" alt="">
-  <div class="movie-info">
-    <h3>Movie Title</h3>
-    <span class="green"><i class="bi bi-heart" id="unlike"></i>8.5</span>
+const display = (movies) => {
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average } = movie;
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+          <h4>${title}</h4>
+        </div>
+  <div class="interact">
+          <button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal">Comment<i class="bi bi-chat"></i></button>
+          <span class="${getClassByRate(vote_average)} span">
+          <i class="bi bi-heart unlike"></i>
+          <i class="bi bi-heart-fill d-none like"></i>
+          ${vote_average}
+        </span>
   </div>
-  <div class="overview">
-    <h3>Overview</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam mollitia fugit nostrum voluptas voluptatum?
-      Aut at saepe quis vero facere!</p>
-  </div>
-</div>`;
+      `;
+    container.appendChild(movieEl);
+  });
 };
+
+function getClassByRate(vote) {
+  if (vote >= 7) {
+    return 'green';
+  } if (vote >= 5) {
+    return 'orange';
+  }
+  return 'red';
+}
+
+export default display;
