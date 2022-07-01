47704c0dd12b404562238544a134cc1e04855d20
Farida-Hamid <faridaaddala@gmail.com>
2022-07-01 13:46:09 +0300

    Remove consoles

--- src/modules/getlikes.js
+++ src/modules/getlikes.js
@@ -8,18 +8,14 @@ const getLikes = async () => {
   const moviesLikes = document.querySelectorAll('.count-likes');
 
   for (let i = 0; i < moviesLikes.length; i += 1) {
-    console.log('searching for movie', moviesLikes[i].id)
     let flag = false;
     for (let j = 0; j < likesCount.length; j += 1) {
       if(moviesLikes[i].id === likesCount[j].item_id){
-        console.log(i, likesCount[j].item_id, likesCount[j].likes);
         flag = true;
-        moviesLikes[i].textContent = likesCount[j].likes;
       }
     }
     if(!flag){
       moviesLikes[i].textContent = 0;
-      console.log("no likes", moviesLikes[i].textContent);
     }
   };
 }
