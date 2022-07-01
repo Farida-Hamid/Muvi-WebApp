a8736f0cd068d573805bd286915da26d3fc42176
Farida-Hamid <faridaaddala@gmail.com>
2022-07-01 13:42:35 +0300

    Remove redundant code

--- src/modules/getlikes.js
+++ src/modules/getlikes.js
@@ -4,10 +4,8 @@ const getLikes = async () => {
   const request = new Request(likesURL);
   const response = await fetch(request);
   const likesCount = await response.json();
-  // console.log('likesCount = ', likesCount);
 
   const moviesLikes = document.querySelectorAll('.count-likes');
-  // console.log('moviesLikes = ', moviesLikes, "len =", moviesLikes.length);
 
   for (let i = 0; i < moviesLikes.length; i += 1) {
     console.log('searching for movie', moviesLikes[i].id)
@@ -18,19 +16,12 @@ const getLikes = async () => {
         flag = true;
         moviesLikes[i].textContent = likesCount[j].likes;
       }
-
-      // else {
-      //   console.log("no likes", i);
-      // }
     }
     if(!flag){
       moviesLikes[i].textContent = 0;
       console.log("no likes", moviesLikes[i].textContent);
     }
-    // if (moviesLikes[i].id === likesCount[i].item_id) {
-    //   
-    // }
-  }
-};
+  };
+}
 
 export default getLikes;
