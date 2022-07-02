13530ef2493900df004487d7db00a60654f0f210
Farida-Hamid <faridaaddala@gmail.com>
2022-07-02 20:27:14 +0300

    Fix Linter error

--- src/modules/getMovies.js
+++ src/modules/getMovies.js
@@ -1,5 +1,5 @@
 import display from './display.js';
-import movieCount from './movieCounter.js'
+import movieCount from './movieCounter.js';
 
 export const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=24997fed86518e9d29afc6b7f7510f37&page=1';
 export const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=24997fed86518e9d29afc6b7f7510f37&query="';
--- src/modules/movieCounter.test.js
+++ src/modules/movieCounter.test.js
@@ -1,4 +1,4 @@
-import movieCount from './movieCounter.js'
+import movieCount from './movieCounter.js';
 
 test('1. Check comment counter', async () => {
   const data = await movieCount(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
